1eb9fa2cb1e7dd02c84067996a6b535c1ef1fa48
XavierAguirre <xaguirre@espol.edu.ec>
2016-09-09 16:23:55 -0500

    Version final conexion

--- php/clases/conexion.php
+++ php/clases/conexion.php
@@ -1,9 +1,9 @@
 <?php 
 class conexion extends PDO {
-	private $dbname = "examen";
-	private $host = "localhost";
-	private $user = "postgres";
-	private $pass = "root";
+	private $dbname = "d3rcn9tf0th43c";
+	private $host = "ec2-54-163-239-218.compute-1.amazonaws.com";
+	private $user = "ucbwkgsngyrwzc";
+	private $pass = "Q-NmYFBn7VOzcMSAdsoUxNJqYP";
 	private $port = "5432";
 	private $dbh;
  	
